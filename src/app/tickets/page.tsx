a3f31cd4640fe984e63c6e0087b5bed5e111e378
ZIH-LIANG FU <145329577+thumbsupsputter@users.noreply.github.com>
2025-08-28 16:19:19 +0800

    2024-v1-const

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,12 +2,21 @@ import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+const TICKET_ICONS = {
+  OPEN: "🔓",
+  DONE: "🔒",
+  IN_PROGRESS: "🔄",
+};
+
 const TicketsPage = () => {
   return (
     <div>
       <ul>
         {initialTickets.tickets.map((ticket) => (
           <li key={ticket.id}>
+            <span className="text-2xl">
+              {TICKET_ICONS[ticket.status]}
+            </span>
             <h2 className="text-2xl font-bold">{ticket.title}</h2>
             <Link
               href={ticketPath(ticket.id)}
--- src/data.ts
+++ src/data.ts
@@ -4,13 +4,19 @@ export const initialTickets = {
       id: "1",
       title: "Ticket 1",
       content: "Ticket 1 Content",
-      status: "DONE",
+      status: "DONE" as const,
     },
     {
       id: "2",
       title: "Ticket 2",
       content: "Ticket 2 Content",
-      status: "OPEN",
+      status: "OPEN" as const,
+    },
+    {
+      id: "3",
+      title: "Ticket 3",
+      content: "Ticket 3 Content",
+      status: "IN_PROGRESS" as const,
     },
   ],
 };
