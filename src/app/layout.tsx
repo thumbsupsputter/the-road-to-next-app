22f6724b81f4ddb784188c47e2dcc59d27ba185b
ZIH-LIANG FU <145329577+thumbsupsputter@users.noreply.github.com>
2025-08-29 00:50:31 +0800

    button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = Geist({
@@ -31,14 +32,14 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Link href={homePath} className="text-2xl font-bold">
-              Home
-            </Link>
+            <Button asChild variant="outline">
+              <Link href={homePath}>Home</Link>
+            </Button>
           </div>
           <div>
-            <Link href={ticketsPath} className="text-2xl font-bold">
-              Tickets
-            </Link>
+            <Button asChild variant="outline">
+              <Link href={ticketsPath}>Tickets</Link>
+            </Button>
           </div>
         </nav>
         <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
