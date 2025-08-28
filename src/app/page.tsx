1fd7eebf49ff24ed5e0b734ff47131bcc0264d6e
ZIH-LIANG FU <145329577+thumbsupsputter@users.noreply.github.com>
2025-08-28 17:53:04 +0800

    2024-v1-tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,21 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home, Page</h1>
-      <Link href={ticketsPath} className="underline">
-        Go Tickets
-      </Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div className="">
+        <h1 className="text-3xl font-bold tracking-tight">
+          Home Page
+        </h1>
+
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath} className="underline">
+          Go Tickets
+        </Link>
+      </div>
     </div>
   );
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -10,14 +10,31 @@ const TICKET_ICONS = {
 
 const TicketsPage = () => {
   return (
-    <div>
-      <ul>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div className="">
+        <h1 className="text-3xl font-bold tracking-tight">
+          Tickets Page
+        </h1>
+
+        <p className="text-sm text-muted-foreground">
+          All your tickets at one place
+        </p>
+      </div>
+      <ul className="flex-1 flex flex-col items-center gap-y-4">
         {initialTickets.tickets.map((ticket) => (
-          <li key={ticket.id}>
+          <li
+            key={ticket.id}
+            className="w-full max-w-[420px] p-4 border border-slate-300 rounded-md"
+          >
             <span className="text-2xl">
               {TICKET_ICONS[ticket.status]}
             </span>
-            <h2 className="text-2xl font-bold">{ticket.title}</h2>
+            <h2 className="text-2xl font-semibold truncate">
+              {ticket.title}
+            </h2>
+            <p className="text-sm text-slate-500 truncate">
+              {ticket.content}
+            </p>
             <Link
               href={ticketPath(ticket.id)}
               className="text-sm underline"
